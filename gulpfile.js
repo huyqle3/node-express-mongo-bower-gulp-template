73a6ede5d4ccadad48cf831f2516189af432168d
huyle333 <huyle333@bu.edu>
2016-05-23 13:59:51 -0400

    [HL] Updated because of font-awesome typo.

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,33 @@ var notify = require("gulp-notify");
 var bower = require('gulp-bower');
 
 var config = {
-bootstrapDir: './bower_components/bootstrap-sass',
-fontawesomeDir: './bower_components/font-awesome',
-publicDir: './public',
-bowerDir: './bower_components'
+	bootstrapDir: './bower_components/bootstrap-sass',
+	fontawesomeDir: './bower_components/font-awesome',
+	publicDir: './public',
+	bowerDir: './bower_components'
 }
 
 gulp.task('bower', function() {
-return bower()
-.pipe(gulp.dest(config.bowerDir))
+	return bower()
+		.pipe(gulp.dest(config.bowerDir))
 });
 
 gulp.task('fonts', function() {
-return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
-.pipe(gulp.dest('./public/fonts'));
+	return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+		.pipe(gulp.dest('./public/fonts'));
 });
 
 gulp.task('css', function() {
-return gulp.src('./public/sass/style.scss')
-.pipe(sass({
-includePaths: [config.bootstrapDir + '/assets/stylesheets', config.fontawesomeDir + '/scss'],
-}))
-.pipe(gulp.dest(config.publicDir + '/stylesheets'));
+	return gulp.src('./public/sass/style.scss')
+		.pipe(sass({
+			includePaths: [config.bootstrapDir + '/assets/stylesheets', config.fontawesomeDir + '/scss'],
+		}))
+		.pipe(gulp.dest(config.publicDir + '/stylesheets'));
 });
 
 // Watch task
 gulp.task('watch',function() {
-gulp.watch('./public/sass/**/*.scss', ['css']);
+	gulp.watch('./public/sass/**/*.scss', ['css']);
 });
 
 gulp.task('default', ['bower', 'fonts', 'css']);
